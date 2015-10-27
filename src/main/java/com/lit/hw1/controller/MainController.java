f2caf076fa159085ba585ad6c0199d59db3c8a04
elit69 <chhornelit@gmail.com>
2015-10-27 23:23:34 +0700

    re-re-initilize

--- src/main/java/com/lit/hw1/controller/MainController.java
+++ src/main/java/com/lit/hw1/controller/MainController.java
@@ -16,7 +16,7 @@ public class MainController {
 	@Autowired
 	StudentDao studentDao;
 	
-	@RequestMapping(value = "/" ,  method = RequestMethod.GET)
+	@RequestMapping(value = {"/","home"} ,  method = RequestMethod.GET)
 	public String homePage(ModelMap model){
 		model.addAttribute("message","Hello Home Page");
 		model.addAttribute("listStudent", studentDao.list());
@@ -28,12 +28,6 @@ public class MainController {
 		model.addAttribute("message","This is Add Page");
 		return "add";
 	}
-
-	@RequestMapping(value = "add" ,  method = RequestMethod.POST)
-	public String add(ModelMap model){
-		model.addAttribute("student" , new Student());
-		return "home";
-	}
 	
 	@RequestMapping(value = "update" ,  method = RequestMethod.GET)
 	public String updatePage(ModelMap model){
@@ -41,19 +35,6 @@ public class MainController {
 		return "update";
 	}
 	
-	@RequestMapping(value = "update" ,  method = RequestMethod.POST)
-	public String update(ModelMap model){
-		model.addAttribute("student" , new Student());
-		return "home";
-	}
-
-	@RequestMapping(value = "delete" ,  method = RequestMethod.DELETE)
-	public String delete(@PathVariable int ownerId, @PathVariable int petId){
-		this.clinic.deletePet(petId);
-		return "redirect:/owners/" + ownerId;
-	}
-
-
 	@RequestMapping(value = "detail" ,  method = RequestMethod.GET)
 	public String detailPage(ModelMap model){
 		model.addAttribute("message","This is Detail Page");
