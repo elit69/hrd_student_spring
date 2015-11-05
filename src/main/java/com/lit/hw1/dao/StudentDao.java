6a1bd8878804953463f594f4fdaa8335c11bd51b
Chhorn Elit <chhornelit@gmail.com>
2015-11-05 08:36:30 +0700

    try with resource that works

--- src/main/java/com/lit/hw1/dao/StudentDao.java
+++ src/main/java/com/lit/hw1/dao/StudentDao.java
@@ -3,30 +3,24 @@ package com.lit.hw1.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-
 import javax.sql.DataSource;
 
 import com.lit.hw1.dto.Student;
 
-public class StudentDao {
+public class StudentDao{
 	private DataSource dataSource;
-	private Connection cnn;
 
 	public StudentDao(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 
 	public ArrayList<Student> list() {
-		ResultSet rs = null;
-		PreparedStatement ps = null;
-		try {
-			cnn = dataSource.getConnection();
-			String sql = "select * from student ORDER BY id;";
-			ps = cnn.prepareStatement(sql);
+		String sql = "select * from student ORDER BY id;";
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
 			System.out.println(ps.toString());
-			rs = ps.executeQuery();
 			ArrayList<Student> listStudent = new ArrayList<Student>();
 			while (rs.next()) {
 				Student s = new Student();
@@ -39,23 +33,15 @@ public class StudentDao {
 			return listStudent;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				rs.close();
-				ps.close();
-				cnn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 		return null;
 	}
 
 	public boolean add(Student stu) {
-		PreparedStatement ps = null;
-		try {
-			cnn = dataSource.getConnection();
-			ps = cnn.prepareStatement("INSERT INTO student(first_name,last_name,classroom) " + "VALUES (?,?,?);");
+		String sql = "INSERT INTO student(first_name,last_name,classroom) "
+				+ "VALUES (?,?,?);";
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setString(1, stu.getFirstname());
 			ps.setString(2, stu.getLastname());
 			ps.setString(3, stu.getClassroom());
@@ -64,23 +50,15 @@ public class StudentDao {
 				return true;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				ps.close();
-				cnn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 		return false;
 	}
 
 	public boolean update(Student stu) {
-		PreparedStatement ps = null;
-		try {
-			cnn = dataSource.getConnection();
-			ps = cnn.prepareStatement(
-					"update student set " + "first_name = ?, " + "last_name = ?, " + "classroom= ? " + "where id = ?");
+		String sql = "update student set " + "first_name = ?, "
+				+ "last_name = ?, " + "classroom= ? " + "where id = ?";
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setString(1, stu.getFirstname());
 			ps.setString(2, stu.getLastname());
 			ps.setString(3, stu.getClassroom());
@@ -90,49 +68,31 @@ public class StudentDao {
 				return true;
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			try {
-				ps.close();
-				cnn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 		return false;
 	}
 
 	public boolean delete(int stuId) {
-		PreparedStatement ps = null;
-		try {
-			cnn = dataSource.getConnection();
-			ps = cnn.prepareStatement("Delete from student where id = ?");
+		String sql = "Delete from student where id = ?";
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setInt(1, stuId);
 			System.out.println(ps.toString());
 			if (ps.executeUpdate() > 0)
 				return true;
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			try {
-				ps.close();
-				cnn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 		return false;
 	}
 
 	public Student show(int stuId) {
-		ResultSet rs = null;
-		PreparedStatement ps = null;
-		try {
-			cnn = dataSource.getConnection();
-			String sql = "select * from student where id = ?";
-			ps = cnn.prepareStatement(sql);
+		String sql = "select * from student where id = ?";
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setInt(1, stuId);
+			ResultSet rs = ps.executeQuery();
 			System.out.println(ps.toString());
-			rs = ps.executeQuery();
 			if (rs.next()) {
 				Student s = new Student();
 				s.setId(rs.getInt("id"));
@@ -143,28 +103,18 @@ public class StudentDao {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				rs.close();
-				ps.close();
-				cnn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 		return null;
 	}
 
-	public ArrayList<Student> search(String keyword, String type){
-		ResultSet rs = null;
-		PreparedStatement ps = null;
-		try {
-			cnn = dataSource.getConnection();
-			String sql = "select * from student where LOWER("+ type +") like LOWER(?) ORDER BY " + type;
-			ps = cnn.prepareStatement(sql);
+	public ArrayList<Student> search(String keyword, String type) {
+		String sql = "select * from student where LOWER(" + type
+				+ ") like LOWER(?) ORDER BY " + type;
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			System.out.println(ps.toString());
 			ps.setString(1, keyword + "%");
-			rs = ps.executeQuery();
+			ResultSet rs = ps.executeQuery();
 			ArrayList<Student> listStudent = new ArrayList<Student>();
 			while (rs.next()) {
 				Student s = new Student();
@@ -177,14 +127,6 @@ public class StudentDao {
 			return listStudent;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				rs.close();
-				ps.close();
-				cnn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 		return null;
 	}
